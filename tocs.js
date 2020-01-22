fa0deef4559aa03c30a0fa7033012ecd3a99f8bf
Maksym Semenykhin <msem@isddesign.com>
2020-01-22 17:04:04 +0200

    Set _disabled true for bad default attribute

--- tocs.js
+++ tocs.js
@@ -16,9 +16,11 @@ const possibleCases = [1, 2, 3, 4];
  * @typedef {Object} TocObject
  */
 
-class TocObject {
+class TocObject
+{
 
-  constructor(tableOfContents) {
+  constructor(tableOfContents)
+  {
     this._disabled = true;
     this._text = null;
     this._id = null;
@@ -28,27 +30,42 @@ class TocObject {
 
     let _tocValue = null;
 
-    if (IN_ARRAY(typeof tableOfContents, ['object', 'array'])) {
-      Object.keys(tableOfContents).forEach((element) => {
-        if(typeof tableOfContents[element] !=='undefined' && typeof tableOfContents[element]['name'] !== 'undefined' ){
+    if (IN_ARRAY(typeof tableOfContents, ['object', 'array']))
+    {
+      Object.keys(tableOfContents).forEach((element) =>
+      {
+        if (typeof tableOfContents[element] !== 'undefined' && typeof tableOfContents[element]['name'] !== 'undefined')
+        {
           this._attributes[tableOfContents[element].name] = tableOfContents[element].value;
-        }else{
+        }
+        else
+        {
           this._attributes[element] = tableOfContents[element];
         }
       });
-    } else {
+    }
+    else
+    {
       this._attributes['default'] = tableOfContents;
     }
 
-    if (this.attrExist('isInToc') && this.getAttr('isInToc') === 'on') {
+    if (this.attrExist('isInToc') && this.getAttr('isInToc') === 'on')
+    {
       this._disabled = false;
-      if (this.attrExist('tocLevel')) {
+      if (this.attrExist('tocLevel'))
+      {
         _tocValue = this.getAttr('tocLevel');
       }
-    } else {
-      if (this.attrExist('default')) {
+    }
+    else
+    {
+      if (this.attrExist('default'))
+      {
         _tocValue = this.getAttr('default');
         this._disabled = false;
+      }else{
+        this._disabled = true;
+
       }
     }
 
@@ -56,50 +73,63 @@ class TocObject {
 
   }
 
-  getAttr(attribute) {
-    if(this.attrExist(attribute)){
+  getAttr(attribute)
+  {
+    if (this.attrExist(attribute))
+    {
       return this._attributes[attribute];
     }
 
     return null;
   }
 
-  attrExist(attribute) {
+  attrExist(attribute)
+  {
     return typeof this._attributes[attribute] !== 'undefined';
   }
 
-  setLand(lang) {
-    if (typeof lang === "string" && lang.length > 0) {
+  setLand(lang)
+  {
+    if (typeof lang === "string" && lang.length > 0)
+    {
       this._lang = lang;
     }
   }
 
-  setText(text) {
-    if (typeof text === "string" && text.length > 0) {
+  setText(text)
+  {
+    if (typeof text === "string" && text.length > 0)
+    {
       this._text = text;
     }
   }
 
-  setId(id) {
-    if (typeof id === "string" && id.length > 0) {
+  setId(id)
+  {
+    if (typeof id === "string" && id.length > 0)
+    {
       this._id = id;
     }
   }
 
-  setIndentation(indentation) {
-    if (!indentation) {
+  setIndentation(indentation)
+  {
+    if (!indentation)
+    {
       return;
     }
 
     indentation = indentation.replace(
       new RegExp('\\w+(\\d+)', 'g'),
-      (groupMatch, match) => {
+      (groupMatch, match) =>
+      {
         return match;
       }
     );
 
     indentation = parseInt(indentation);
-    if (possibleCases.includes(indentation)) {
+    if (possibleCases.includes(indentation))
+    {
       this._indentation = indentation;
     }
   }
@@ -107,24 +137,29 @@ class TocObject {
   /**
    * @return {Toc}
    */
-  getJson() {
+  getJson()
+  {
 
     let tocObject = {};
 
-    if (this._text !== null) {
+    if (this._text !== null)
+    {
       tocObject.text = this._text;
     }
     tocObject.styles = [];
 
-    if (this._id !== null) {
+    if (this._id !== null)
+    {
       tocObject.id = this._id;
     }
 
-    if (this._indentation !== null) {
+    if (this._indentation !== null)
+    {
       tocObject.indentation = this._indentation;
     }
 
-    if (this._lang !== null) {
+    if (this._lang !== null)
+    {
       tocObject.lang = this._lang;
     }
 
@@ -135,11 +170,13 @@ class TocObject {
   /**
    * @return {Boolean}
    */
-  isEmpty() {
+  isEmpty()
+  {
     return this._disabled === true || !this._text;
   }
 
 }
+
 
 module.exports = {
   TocObject
